779f035dcc89ad89aabba5675786965d16db77ef
Danny Rooker <dannyrooks@gmail.com>
2019-07-12 14:03:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,13 @@
-# def nyc_pigeon_organizer(pigeon_data)
-#   new_pigeon_hash = {}
-#   pigeon_array = []
-#   pigeon_data.map do |key, value|
-#     value.map do |sub_value, names|
-#       names.map do |name|
-#         if !pigeon_array.include?(name)
-#           pigeon_array << name
+def nyc_pigeon_organizer(pigeon_data)
+  new_pigeon_hash = {}
+  pigeon_array = []
+  pigeon_data.map do |key, value|
+    value.map do |sub_value, names|
+      names.map do |name|
+        if !pigeon_array.include?(name)
+          pigeon_array << name
   
-#     end
-#     end
-#   end
-# end
+    end
+    end
+  end
+end
