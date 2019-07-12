2ab057a52859045d3ceafd7e3e90b240910252fb
Danny Rooker <dannyrooks@gmail.com>
2019-07-12 14:15:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,11 +3,11 @@ def nyc_pigeon_organizer(data)
     data.map {|key, value|
       value.map {|traits, names|
     name.map {|name|
-  pigeon_update[name] ||= {}
-  pigeon_update[name][key] ||=[]
-  pigeon_update[name][key] << details.to_s 
-  }
-  }
+    pigeon_update[name] ||= {}
+    pigeon_update[name][key] ||= []
+    pigeon_update[name][key] << details.to_s
+      }
+    }
   } 
   return pigeon_update
 end
