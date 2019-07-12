3043edba601d4af109c32e6cf3a52670b125c434
Danny Rooker <dannyrooks@gmail.com>
2019-07-12 14:18:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_update = {}
+    data.map { |key, value|
+      value.map { |traits, names|
+    names.map { |name|
+      pigeon_update[name] ||= {}
+      pigeon_update[name][key] ||= []
+       pigeon_update[name][key] << details.to_s
+    }
+  } 
+end
+  return pigeon_update
 end
