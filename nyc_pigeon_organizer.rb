23f40257a118223882311bb79a485cc1509a8ce5
Danny Rooker <dannyrooks@gmail.com>
2019-07-12 14:03:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,3 +11,19 @@ def nyc_pigeon_organizer(pigeon_data)
     end
   end
 end
+
+  array_of_pigeons.map do |individual_name|
+    final_pigeon_hash[individual_name] = Hash.new {|attribute, quality| attribute[quality] =[]}
+    pigeon_data.map do |metadata_color_gender_lives, intricate_variable_info|
+      final_pigeon_hash[individual_name][metadata_color_gender_lives]
+      intricate_variable_info.map do |intricate_variable, pigeon_names|
+        pigeon_names.map do |names|
+          if names == individual_name
+            final_pigeon_hash[individual_name][metadata_color_gender_lives] << intricate_variable.to_s
+          end
+        end
+      end
+    end
+  end
+final_pigeon_hash
+end
